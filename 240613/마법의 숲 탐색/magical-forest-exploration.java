7a6a9a26fb014a03734668c4a0f4e265d0c96be0
asever0318 <35682216+tlalswn23@users.noreply.github.com>
2024-06-13 20:43:51 +0900

    Title: 마법의 숲 탐색, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240613/마법의 숲 탐색/magical-forest-exploration.java
+++ 240613/마법의 숲 탐색/magical-forest-exploration.java
@@ -5,8 +5,6 @@ public class Main {
 	static int R, C, K, total;
 	static int[][] map, exit;
 	static int[][] direction = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-	static int[][] left = {{-1, 0}, {0, -1}, {1, 0}, {1, -1}, {1, 1}, {2, 0}};
-	static int[][] right = {{-1, 0}, {0, 1}, {-1, 0}, {1, 1}, {1, -1}, {2, 0}};
 	static int[][] check = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 0}, {0, 1}, {1, 0}};
 	
 	public static void main(String[] args) throws IOException{
@@ -14,11 +12,11 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		R = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken()) + 3;
 		C = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		map = new int[R+1][C+1];
-		exit = new int[R+1][C+1];
+		map = new int[R+3][C+1];
+		exit = new int[R+3][C+1];
 		
 		for(int i = 1; i < K+1; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -38,27 +36,20 @@ public class Main {
 	static void moveGolem(int r, int c, int d, int n) {
 		
 		// 1. 밑으로 내려가기 
-		if(canGo(r+1, c, d, n)) {
+		if(canGo(r+1, c)) {
 			moveGolem(r+1, c, d, n);
 			return;
 		}
 		
 		// 2. 왼쪽으로 내려가기 
-		if(canGoLeft(r, c-1)) {
-			if(d == 0) {
-				d = 4;
-			}
-			
-			moveGolem(r+1, c-1, d-1, n);
+		if(canGo(r+1, c-1)) {
+			moveGolem(r+1, c-1, (d+3)%4, n);
 			return;
 		}
 		
 		// 3. 오른쪽으로 내려가기 
-		if(canGoRight(r, c+1)) {
-			if(d == 3) {
-				d = -1;
-			}
-			moveGolem(r+1, c+1, d+1, n);
+		if(canGo(r+1, c+1)) {
+			moveGolem(r+1, c+1, (d+1)%4, n);
 			return;
 		}
 		
@@ -76,7 +67,7 @@ public class Main {
 			exit[r + direction[d][0]][c + direction[d][1]] = 1; // 출구 표시 
 			
 			// 골렘 층수 계산 
-			int row = findGolem(r, c, n);
+			int row = findGolem(r, c, n) - 3;
 			total += row; // 골렘 위치 찾는 함수 
 			
 //			System.out.println(row);
@@ -90,7 +81,7 @@ public class Main {
 			int nr = r + direction[d][0];
 			int nc = c + direction[d][1];
 			
-			if(nr < 1 || nr > R || nc < 1 || nc > C) {
+			if(nr < 3 || nr > R || nc < 1 || nc > C) {
 				return false;
 			}
 		}
@@ -98,7 +89,7 @@ public class Main {
 		return true;
 	}
 	
-	static boolean canGo(int r, int c, int d, int n) {
+	static boolean canGo(int r, int c) {
 		for(int i = 0; i < 7; i++) {
 			int nr = r + check[i][0];
 			int nc = c + check[i][1];
@@ -114,42 +105,8 @@ public class Main {
 		
 		return true;
 	}
-	
-	static boolean canGoLeft(int r, int c) {
-		
-		for(int i = 0; i < 6; i++) {
-			int nr = r + left[i][0];
-			int nc = c + left[i][1];
-			
-			if(nr < 0 || nr > R || nc < 1 || nc > C) {
-				return false;
-			}
-			
-			if(map[nr][nc] != 0) {
-				return false;
-			}
-		}
-		
-		return true;
-	}
-	
-	static boolean canGoRight(int r, int c) {
-		
-		for(int i = 0; i < 6; i++) {
-			int nr = r + right[i][0];
-			int nc = c + right[i][1];
-			
-			if(nr < 0 || nr > R || nc < 1 || nc > C) {
-				return false;
-			}
-			
-			if(map[nr][nc] != 0) {
-				return false;
-			}
-		}
-		
-		return true;
-	}
+
+
 	// 골렘의 위치 찾기 - BFS
 	static int findGolem(int r, int c, int n) {
 		boolean[][] visited = new boolean[R+1][C+1];
@@ -165,7 +122,7 @@ public class Main {
 				int nr = current[0] + direction[d][0];
 				int nc = current[1] + direction[d][1];
 				
-				if(nr < 1 || nr > R || nc < 1 || nc > C) {
+				if(nr < 3 || nr > R || nc < 1 || nc > C) {
 					continue;
 				}
 				
@@ -200,7 +157,7 @@ public class Main {
 	}
 	
 	static void printMap(int[][] arr) {
-		for(int i = 0; i <= R; i++) {
+		for(int i = 1; i <= R; i++) {
 			for(int j = 1; j <= C; j++) {
 				System.out.print(arr[i][j]+" ");
 			}
